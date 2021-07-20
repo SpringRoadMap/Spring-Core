b068f116d0da6f8c6312d8341ad790b144949923
PandaHun <gnsgnsek@gmail.com>
2021-07-20 15:42:45 +0900

    test: Create signleton, singleton container test

---
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -0,0 +1,39 @@
+package hello.core.singleton;
+
+import hello.core.AppConfig;
+import hello.core.member.MemberService;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("순수한 DI 컨테이너")
+    void pureContainerTest() {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService1 = appConfig.memberService();
+        MemberService memberService2 = appConfig.memberService();
+        assertThat(memberService1.equals(memberService2)).isFalse();
+    }
+
+    @Test
+    @DisplayName("싱글톤 서비스 객체 사용")
+    void singletonTest() {
+        SingletonService singletonService1 = SingletonService.getInstance();
+        SingletonService singletonService2 = SingletonService.getInstance();
+        assertThat(singletonService1).isSameAs(singletonService2);
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너 사용")
+    void springContainerTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+        assertThat(memberService1).isEqualTo(memberService2);
+    }
+}
